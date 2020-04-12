bb2c22017c7bf17510237729c5bd8bebff43abbd
mathe <santiosmatheus@gmail.com>
2020-04-12 16:53:51 -0400

    Projects

---
+++ Aulas.html/aula011.js
@@ -0,0 +1,11 @@
+console.log("O console funcionou corretamente!")
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é: ${vel}km/h`)
+
+console.log("Diriga sempre usando sinto de segurança.")
+
+
+if (vel>=60.5) {
+    console.log("voce passou do limite")
+} 
---
+++ Aulas.html/ex011.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var semana = agora.getDay()
+switch(semana){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça-Feira')
+        break
+    case 3:
+        console.log('Quarta-Feira')
+        break
+    case 4:
+        console.log('Quinta-Feira')
+        break
+    case 5:
+        console.log('Sexta-Feira')
+    case 6:
+        console.log('Sábado')
+        break
+}
+
+
